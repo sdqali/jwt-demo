41ffbdfcb351df255873f9b84f53b9eacd2e1d9d
Sadique Ali <sadiqalikm@gmail.com>
2016-07-05 22:44:42 -0700

    Selectively disable CSRF

--- src/main/java/in/sdqali/spring/jwt/demo/configuration/SecurityConfig.java
+++ src/main/java/in/sdqali/spring/jwt/demo/configuration/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().ignoringAntMatchers("/login");
 
         http.authorizeRequests()
                 .antMatchers("/login")
