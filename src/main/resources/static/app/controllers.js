7d540b12c4ebdf265d0f9c8c966153229edc79c3
Sadique Ali <sadiqalikm@gmail.com>
2016-07-10 14:05:24 -0700

    Save JWT in the root scope.

--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -1,16 +1,17 @@
 (function(angular) {
-  var AppController = function($scope, Login) {
+  var AppController = function($scope, $rootScope, Login) {
     $scope.profile = {};
     $scope.login = function(username, password) {
       new Login({username: username, password: password})
-          .$login(function (profile) {
+          .$login(function (profile, headers) {
             $scope.profile = profile;
+            $rootScope.jwt = headers().token;
           }, function (error) {
             console.log(error);
           });
     };
   };
 
-  AppController.$inject = ['$scope', 'Login'];
+  AppController.$inject = ['$scope', '$rootScope', 'Login'];
   angular.module("jwtDemo.controllers").controller("AppController", AppController);
 }(angular));
